ed3c4bc1761c42ccba7e7ce11a00f8910f5c4cb9
TSUJ-07 <tsuwav@gmail.com>
2025-04-12 11:22:45 -0400

    Config Settings Reminders #Add sprites---later #Worry about configuring speed over time of display---later #Figure out how to create a main menu before game start? ---later #Images!! background image maybe ---later #Sounds!! Vrroom Vrroom sounds!! Honk Honk sounds!! ---later #Animating the road? Is that even possible? ---later ...

---
+++ GameFolder/Config.py
@@ -0,0 +1,39 @@
+import pygame, random
+pygame.init()
+
+#Settings
+RESOLUTION = (500, 750)
+FPS= 60
+XEVENT = pygame.USEREVENT +1
+
+BLACK= (0,0,0)
+RED= (255,0,0)
+BLUE= (0,0,255)
+GREEN= (0,255,0)
+
+#Player Settings
+
+PLAYER_POS= (225,625)
+PLAYER_WIDTH= 50
+PLAYER_HEIGHT= 100
+PLAYER_SPEED= 5
+#---Player_Sprites---#
+#Insert{}
+
+#Obstacle Settings
+TRAFF_WIDTH= 50
+TRAFF_HEIGHT= 100
+TRAFF_LANES= [100,200,300,400]
+MIN_SPEED=2
+MAX_SPEED=6
+#---Obstacle_Sprites---#
+#Insert{}
+
+#Road Display
+GRASS= 50
+ROAD_SIZE= RESOLUTION[0] - (GRASS *2)
+#---Random speed for traffic---#
+def car_speed_random():
+    return random.randint(MIN_SPEED, MAX_SPEED)
+def screen():
+    return pygame.display.set_mode(RESOLUTION)
